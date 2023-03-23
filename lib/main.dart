3645bb2a83d3bcda9e9b999d921fc1f234cfc6fa
robinyeon <robin.yeon@gmail.com>
2023-03-23 16:28:41 +0900

    Dialog

--- lib/main.dart
+++ lib/main.dart
@@ -1,51 +1,115 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(home: MyApp()));
 }
 
 class Footer extends StatelessWidget {
-  const Footer({Key? key}) : super(key: key);
+  Footer({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.fromLTRB(0, 10, 0, 10),
-      width: double.infinity,
+      margin: EdgeInsets.fromLTRB(0, 15, 0, 15),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [Icon(Icons.phone), Icon(Icons.chat_bubble), Icon(Icons.person_2_rounded)],
-      )
+        children: [
+          Icon(Icons.phone),
+          Icon(Icons.chat_bubble),
+          Icon(Icons.person)
+        ],
+      ),
     );
   }
 }
 
-
-class MyApp extends StatelessWidget {
-  MyApp({Key? key}) : super(key: key);
-  var cnt = 0;
+class DialogUI extends StatelessWidget {
+  const DialogUI({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    return Dialog(
+        child: Container(
+      padding: EdgeInsets.all(20),
+      width: 400,
+      height: 250,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            margin: EdgeInsets.only(bottom: 20),
+            child: TextFormField(
+              decoration: InputDecoration(
+                hintText: 'Enter your name',
+              ),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      primary: Colors.red, fixedSize: Size(80, 40)),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text('취소')),
+              ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(fixedSize: Size(80, 40)),
+                  child: Text('완료'))
+            ],
+          )
+        ],
+      ),
+    ));
+  }
+}
 
-    return MaterialApp(
-     home: Scaffold(
-       floatingActionButton: FloatingActionButton(
-         child: Text('뽀짝'),
-         onPressed: () { cnt++; print(cnt);},
-       ),
-       appBar: AppBar(),
-       body: ListView.builder(
-           itemCount: 3,
-           itemBuilder: (context, index){
-             return ListTile(
-               leading: Icon(Icons.pets),
-               title: Text('연라푸')
-             );
-           }
-       ),
-       bottomNavigationBar:BottomAppBar( child: Footer()),
-     )
-    );
+class MyApp extends StatefulWidget {
+  MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var likeCnts = [0, 0, 0];
+  var name = ['연다은봄', '연라푸', '연똥개'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        floatingActionButton: FloatingActionButton(
+          child: Text('Pop-up'),
+          onPressed: () {
+            showDialog(
+                context: context,
+                builder: (context) {
+                  return DialogUI();
+                });
+          },
+        ),
+        appBar: AppBar(),
+        body: ListView.builder(
+          itemCount: 3,
+          itemBuilder: (context, idx) {
+            return ListTile(
+              leading: Text(likeCnts[idx].toString()),
+              title: Text(name[idx]),
+              trailing: ElevatedButton(
+                child: Text('좋아요'),
+                onPressed: () {
+                  setState(() {
+                    likeCnts[idx]++;
+                  });
+                },
+              ),
+            );
+          },
+        ),
+        bottomNavigationBar: BottomAppBar(
+          child: Footer(),
+        ));
   }
 }
